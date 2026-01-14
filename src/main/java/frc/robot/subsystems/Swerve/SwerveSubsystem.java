6bad2f724684829ac208fa7c0efe9418e82cd006
Owen Leber <owen.m.leber@gmail.com>
2026-01-13 20:53:10 -0500

    Coding Style Settings and improvements

--- src/main/java/frc/robot/subsystems/Swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/Swerve/SwerveSubsystem.java
@@ -1,54 +1,55 @@
-
 package frc.robot.subsystems.Swerve;
-
-import java.io.File;
-import java.io.IOException;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import java.io.File;
+import java.io.IOException;
 import swervelib.SwerveDrive;
 import swervelib.SwerveInputStream;
 import swervelib.parser.SwerveParser;
 
 public class SwerveSubsystem extends SubsystemBase {
-    private final SwerveDrive m_SwerveDrive;
+  private final SwerveDrive m_SwerveDrive;
 
-    private final StructPublisher<Pose2d> m_posePublisher = NetworkTableInstance.getDefault()
-            .getTable("SwerveSubsystem")
-            .getStructTopic("swerve/pose", Pose2d.struct)
-            .publish();
-    public SwerveSubsystem() {
-        File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(), "swerve/neo);");
-        try {
-            m_SwerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(1, new Pose2d(1, 4, Rotation2d.kZero));
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
-        }
+  private final StructPublisher<Pose2d> m_posePublisher =
+      NetworkTableInstance.getDefault()
+          .getTable("SwerveSubsystem")
+          .getStructTopic("swerve/pose", Pose2d.struct)
+          .publish();
 
-    public void driveRobotOrientated(ChassisSpeeds speeds) {
-        m_SwerveDrive.drive(speeds);
+  public SwerveSubsystem() {
+    File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(), "swerve/neo);");
+    try {
+      m_SwerveDrive =
+          new SwerveParser(swerveJsonDirectory)
+              .createSwerveDrive(1, new Pose2d(1, 4, Rotation2d.kZero));
+    } catch (IOException ex) {
+      throw new RuntimeException(ex);
     }
+  }
 
-    public void driveFieldOrientated(ChassisSpeeds speeds) {
-        m_SwerveDrive.driveFieldOriented(speeds);
-    }
+  public void driveRobotOrientated(ChassisSpeeds speeds) {
+    m_SwerveDrive.drive(speeds);
+  }
 
-    public SwerveInputStream getInputStream(CommandXboxController controller) {
-        return SwerveInputStream.of(m_SwerveDrive, ()-> controller.getLeftY() * -1, ()-> controller.getLeftX() * -1);
-    }
+  public void driveFieldOrientated(ChassisSpeeds speeds) {
+    m_SwerveDrive.driveFieldOriented(speeds);
+  }
 
-    @Override
-    public void periodic() {
-        m_SwerveDrive.updateOdometry();
-        m_posePublisher.set(m_SwerveDrive.getPose());
-    }
+  public SwerveInputStream getInputStream(CommandXboxController controller) {
+    return SwerveInputStream.of(
+        m_SwerveDrive, () -> controller.getLeftY() * -1, () -> controller.getLeftX() * -1);
+  }
+
+  @Override
+  public void periodic() {
+    m_SwerveDrive.updateOdometry();
+    m_posePublisher.set(m_SwerveDrive.getPose());
+  }
 }

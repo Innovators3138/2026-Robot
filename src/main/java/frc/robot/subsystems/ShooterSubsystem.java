38ec70289aa51bb2857776634dda35008a7720a9
Jon Guy <jon.guy@live.com>
2026-01-17 11:51:33 -0500

    Implement an example subsystem using YAMS (#13)
    * Implement an example subsystem using YAMS
    
    * Tuning for 3dmechanism feed forward and changed control for flywheel to left trigger
    
    ---------
    
    Co-authored-by: Jon Guy <programmerbits@gmail.com>
    Co-authored-by: charlie guy <charlie.guy@outlook.com>

---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,75 @@
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Pounds;
+import static edu.wpi.first.units.Units.RPM;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.mechanisms.config.FlyWheelConfig;
+import yams.mechanisms.velocity.FlyWheel;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.local.SparkWrapper;
+
+public class ShooterSubsystem extends SubsystemBase {
+  private SmartMotorControllerConfig motorConfig =
+      new SmartMotorControllerConfig(this)
+          .withControlMode(ControlMode.CLOSED_LOOP)
+          .withClosedLoopController(
+              0, 0, 0, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
+          .withSimClosedLoopController(
+              0, 0, 0, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
+          .withFeedforward(new SimpleMotorFeedforward(0, 0.25, 0))
+          .withSimFeedforward(new SimpleMotorFeedforward(0, 0.25, 0))
+          .withTelemetry("ShooterMotor", TelemetryVerbosity.HIGH)
+          .withGearing(new MechanismGearing(GearBox.fromReductionStages(2)))
+          .withMotorInverted(false)
+          .withIdleMode(MotorMode.COAST)
+          .withStatorCurrentLimit(Amps.of(40));
+
+  private SparkMax motor = new SparkMax(4, MotorType.kBrushless);
+
+  private SmartMotorController motorController =
+      new SparkWrapper(motor, DCMotor.getNEO(1), motorConfig);
+
+  private final FlyWheelConfig flywheelConfig =
+      new FlyWheelConfig(motorController)
+          .withDiameter(Inches.of(4))
+          .withMass(Pounds.of(1))
+          .withUpperSoftLimit(RPM.of(2500))
+          .withTelemetry("ShooterMech", TelemetryVerbosity.HIGH);
+
+  private FlyWheel shooter = new FlyWheel(flywheelConfig);
+
+  @Override
+  public void periodic() {
+    shooter.updateTelemetry();
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    shooter.simIterate();
+  }
+
+  public AngularVelocity getAngularVelocity() {
+    return shooter.getSpeed();
+  }
+
+  public Command setAngularVelocity(AngularVelocity angularVelocity) {
+    return shooter.setSpeed(angularVelocity);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -0,0 +1,67 @@
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructPublisher;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import java.io.File;
+import java.io.IOException;
+import swervelib.SwerveDrive;
+import swervelib.SwerveInputStream;
+import swervelib.parser.SwerveParser;
+
+public class SwerveSubsystem extends SubsystemBase {
+  public static LinearVelocity MaxDriveSpeed = MetersPerSecond.of(5);
+  public static AngularVelocity MaxRotationSpeed = RotationsPerSecond.of(3);
+  public static Pose2d InitialPose = new Pose2d(16, 7, Rotation2d.kZero);
+
+  private final SwerveDrive swerveDrive;
+  private final StructPublisher<Pose2d> posePublisher;
+
+  public SwerveSubsystem() {
+    posePublisher =
+        NetworkTableInstance.getDefault()
+            .getStructTopic("Subsystems/Swerve/Pose", Pose2d.struct)
+            .publish();
+
+    File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(), "swerve/neo");
+
+    try {
+      swerveDrive =
+          new SwerveParser(swerveJsonDirectory)
+              .createSwerveDrive(MaxDriveSpeed.in(MetersPerSecond), InitialPose);
+
+      swerveDrive.setMaximumAllowableSpeeds(
+          MaxDriveSpeed.in(MetersPerSecond), MaxRotationSpeed.in(RadiansPerSecond));
+    } catch (IOException ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+
+  public Command driveFieldOriented(CommandXboxController controller) {
+    SwerveInputStream inputStream =
+        SwerveInputStream.of(
+                swerveDrive, () -> controller.getLeftY() * -1, () -> controller.getLeftX() * -1)
+            .withControllerRotationAxis(() -> controller.getRightX() * -1)
+            .deadband(0.1)
+            .allianceRelativeControl(true);
+
+    return run(() -> swerveDrive.driveFieldOriented(inputStream.get()));
+  }
+
+  @Override
+  public void periodic() {
+    swerveDrive.updateOdometry();
+    posePublisher.set(swerveDrive.getPose());
+  }
+}

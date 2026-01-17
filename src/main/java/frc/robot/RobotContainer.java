8910f4e9024d153b170c4f1983d6254f4c4cd6d7
Jon Guy <programmerbits@gmail.com>
2026-01-16 22:48:29 -0500

    Implement an example subsystem using YAMS

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,52 +1,25 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.commands.DriveFieldOrientated;
-import frc.robot.subsystems.Swerve.SwerveSubsystem;
+import static edu.wpi.first.units.Units.RPM;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
+
 public class RobotContainer {
-  private final SwerveSubsystem SwerveSubsystem = new SwerveSubsystem();
+  private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
+  private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
   private final CommandXboxController driverXbox = new CommandXboxController(0);
   private final CommandXboxController operatorXbox = new CommandXboxController(1);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the trigger bindings
     configureBindings();
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   private void configureBindings() {
-    var inputStream =
-        SwerveSubsystem.getInputStream(driverXbox)
-            .withControllerRotationAxis(
-                () -> driverXbox.getRightTriggerAxis() - driverXbox.getLeftTriggerAxis())
-            .scaleRotation(1);
-    SwerveSubsystem.setDefaultCommand(new DriveFieldOrientated(inputStream, SwerveSubsystem));
+    swerveSubsystem.setDefaultCommand(swerveSubsystem.driveFieldOriented(driverXbox));
+    shooterSubsystem.setDefaultCommand(shooterSubsystem.setAngularVelocity(RPM.of(0)));
+
+    operatorXbox.rightTrigger(0.5).whileTrue(shooterSubsystem.setAngularVelocity(RPM.of(60)));
   }
 }
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */

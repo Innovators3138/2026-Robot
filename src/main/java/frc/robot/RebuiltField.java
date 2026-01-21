154d68a0723d66047ba3a2e666a21cce92138672
chargames2011 <91035998+chargames2011@users.noreply.github.com>
2026-01-20 20:18:56 -0500

    Charlie/visionaim (#20)
    * started on the bascs for a vision subsystem
    
    * modified swerve subsystem to allow operater to aim at base

---
+++ src/main/java/frc/robot/RebuiltField.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public class RebuiltField {
+  public static Pose2d BLUE_HUB = new Pose2d(4.341067, 4, Rotation2d.kZero);
+  public static Pose2d RED_HUB = new Pose2d(11.796345, 4.37278, Rotation2d.kZero);
+}

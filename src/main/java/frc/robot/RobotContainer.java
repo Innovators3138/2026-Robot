42a4d61b91e2e7d829f0d94a234f795b20ebcd95
Orion Little <dragonboy022222@gmail.com>
2026-01-22 18:13:50 -0500

    Refactor Climber subsystem: update motor control implementation, remove ClimberSpark class, and enhance joystick commands for height adjustments

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,8 +1,10 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.RPM;
 
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.ClimberSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 
@@ -14,12 +16,20 @@ public class RobotContainer {
 
   public RobotContainer() {
     configureBindings();
+
+    m_climberSubsystem.setDefaultCommand(m_climberSubsystem.setHeight(Meters.of(0)));
   }
+
+  private final ClimberSubsystem m_climberSubsystem = new ClimberSubsystem();
 
   private void configureBindings() {
     swerveSubsystem.setDefaultCommand(swerveSubsystem.driveFieldOriented(driverXbox));
     shooterSubsystem.setDefaultCommand(shooterSubsystem.setAngularVelocity(RPM.of(0)));
 
     operatorXbox.leftTrigger(0.5).whileTrue(shooterSubsystem.setAngularVelocity(RPM.of(2250)));
+    operatorXbox.a().whileTrue(m_climberSubsystem.setHeight(Meters.of(0.5)));
+    operatorXbox.b().whileTrue(m_climberSubsystem.setHeight(Meters.of(1)));
+    operatorXbox.y().whileTrue(m_climberSubsystem.setHeight(Meters.of(0.3)));
+    operatorXbox.x().whileTrue(m_climberSubsystem.setHeight(Meters.of(-0.3)));
   }
 }
--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,31 +1,82 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Pounds;
+import static edu.wpi.first.units.Units.Second;
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
 
-import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj.util.Color8Bit;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.ClimberConstants;
-import org.littletonrobotics.junction.mechanism.LoggedMechanism2d;
-import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d;
-import org.littletonrobotics.junction.mechanism.LoggedMechanismRoot2d;
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.mechanisms.config.ElevatorConfig;
+import yams.mechanisms.positional.Elevator;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.local.SparkWrapper;
 
 public class ClimberSubsystem extends SubsystemBase {
-  public final LoggedMechanism2d mechanism2d =
-      new LoggedMechanism2d(3, 3, new Color8Bit(Color.kBlack));
-  private final LoggedMechanismRoot2d mechRoot2d = mechanism2d.getRoot("Climber Root", 1.5, 0);
-  private final LoggedMechanismLigament2d climberMech2d =
-      mechRoot2d.append(
-          new LoggedMechanismLigament2d("Climber", ClimberConstants.CLIMBER_INITIAL_HEIGHT, null));
+  private SmartMotorControllerConfig smcConfig =
+      new SmartMotorControllerConfig(this)
+          .withControlMode(ControlMode.CLOSED_LOOP)
+          .withMechanismCircumference(Meters.of(Inches.of(0.25).in(Meters) * 22))
+          .withClosedLoopController(
+              4.0, 0.0, 0.0, MetersPerSecond.of(0.5), MetersPerSecondPerSecond.of(0.5))
+          .withSimClosedLoopController(
+              4.0, 0.0, 0.0, MetersPerSecond.of(0.5), MetersPerSecondPerSecond.of(0.5))
+          .withTelemetry("ClimberMotor", TelemetryVerbosity.HIGH)
+          .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
+          .withMotorInverted(false)
+          .withIdleMode(MotorMode.BRAKE)
+          .withStatorCurrentLimit(Amps.of(40))
+          .withClosedLoopRampRate(Second.of(0.25))
+          .withOpenLoopRampRate(Seconds.of(0.25));
 
-  public ClimberSubsystem() {}
+  private SparkMax spark = new SparkMax(23, MotorType.kBrushless);
+
+  private SmartMotorController sparkSmartMotorController =
+      new SparkWrapper(spark, DCMotor.getNEO(1), smcConfig);
+
+  private ElevatorConfig climbconfig =
+      new ElevatorConfig(sparkSmartMotorController)
+          .withStartingHeight(Meters.of(0.5))
+          .withHardLimits(Meters.of(0), Meters.of(3))
+          .withTelemetry("Climber", TelemetryVerbosity.HIGH)
+          .withMass(Pounds.of(16));
+
+  private Elevator climb = new Elevator(climbconfig);
+
+  public Command setHeight(Distance height) {
+    return climb.setHeight(height);
+  }
+
+  public Command set(double dutycycle) {
+    return climb.set(dutycycle);
+  }
+
+  public Command sysId() {
+    return climb.sysId(Volts.of(7), Volts.of(2).per(Second), Seconds.of(4));
+  }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    climb.updateTelemetry();
   }
 
   @Override
   public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+    climb.simIterate();
   }
 }

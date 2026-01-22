bd4a997c83e442ec85396e70536480a0e6ab1d2d
mrO819 <Owen.m.lber@gmail.com>
2026-01-22 18:36:58 -0500

    broken code will fix

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,11 +6,13 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.subsystems.FeederSubsystem;
 
 public class RobotContainer {
   public final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
   public final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
   public final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+  public final FeederSubsystem feederSubsystem = new FeederSubsystem();
   public final CommandXboxController driverXbox = new CommandXboxController(0);
   public final CommandXboxController operatorXbox = new CommandXboxController(1);
 
@@ -22,8 +24,13 @@ public class RobotContainer {
     swerveSubsystem.setDefaultCommand(swerveSubsystem.driveFieldOriented(driverXbox, operatorXbox));
     shooterSubsystem.setDefaultCommand(shooterSubsystem.setAngularVelocity(RPM.of(0)));
     intakeSubsystem.setDefaultCommand(intakeSubsystem.setAngularVelocity(RPM.of(0)));
+    feederSubsystem.setDefaultCommand(feederSubsystem.setFeederAngularVelocity(RPM.of(0)));
+    feederSubsystem.setDefaultCommand(feederSubsystem.setHotdogAngularVelocity(RPM.of(0)));
+
 
     operatorXbox.leftTrigger(0.5).whileTrue(shooterSubsystem.setAngularVelocity(RPM.of(2250)));
+    operatorXbox.rightTrigger(0.5).whileTrue(feederSubsystem.setFeederAngularVelocity(RPM.of(500)));
+    operatorXbox.rightTrigger(0.5).whileTrue(feederSubsystem.setHotdogAngularVelocity(RPM.of(180)));
 
     driverXbox.a().toggleOnTrue(intakeSubsystem.setAngularVelocity(RPM.of(500)));
   }

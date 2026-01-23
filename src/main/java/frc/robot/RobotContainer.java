d4632112d8b9895ac175edf3517b7713bd9063a7
Owen Leber <owen.m.leber@gmail.com>
2026-01-22 20:33:50 -0500

    fixed all problems created new subsystem same button now controls feeder and hotdog

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,16 +3,18 @@ package frc.robot;
 import static edu.wpi.first.units.Units.RPM;
 
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.FeederSubsystem;
+import frc.robot.subsystems.HotdogSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
-import frc.robot.subsystems.FeederSubsystem;
 
 public class RobotContainer {
   public final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
   public final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
   public final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
   public final FeederSubsystem feederSubsystem = new FeederSubsystem();
+  public final HotdogSubsystem hotdogSubsystem = new HotdogSubsystem();
   public final CommandXboxController driverXbox = new CommandXboxController(0);
   public final CommandXboxController operatorXbox = new CommandXboxController(1);
 
@@ -25,12 +27,15 @@ public class RobotContainer {
     shooterSubsystem.setDefaultCommand(shooterSubsystem.setAngularVelocity(RPM.of(0)));
     intakeSubsystem.setDefaultCommand(intakeSubsystem.setAngularVelocity(RPM.of(0)));
     feederSubsystem.setDefaultCommand(feederSubsystem.setFeederAngularVelocity(RPM.of(0)));
-    feederSubsystem.setDefaultCommand(feederSubsystem.setHotdogAngularVelocity(RPM.of(0)));
-
+    hotdogSubsystem.setDefaultCommand(hotdogSubsystem.setHotdogAngularVelocity(RPM.of(0)));
 
     operatorXbox.leftTrigger(0.5).whileTrue(shooterSubsystem.setAngularVelocity(RPM.of(2250)));
-    operatorXbox.rightTrigger(0.5).whileTrue(feederSubsystem.setFeederAngularVelocity(RPM.of(500)));
-    operatorXbox.rightTrigger(0.5).whileTrue(feederSubsystem.setHotdogAngularVelocity(RPM.of(180)));
+    operatorXbox
+        .rightTrigger()
+        .whileTrue(
+            feederSubsystem
+                .setFeederAngularVelocity(RPM.of(500))
+                .alongWith(hotdogSubsystem.setHotdogAngularVelocity(RPM.of(180))));
 
     driverXbox.a().toggleOnTrue(intakeSubsystem.setAngularVelocity(RPM.of(500)));
   }
--- src/main/java/frc/robot/subsystems/HotdogSubsystem.java
+++ src/main/java/frc/robot/subsystems/HotdogSubsystem.java
@@ -13,7 +13,6 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import yams.gearing.GearBox;
 import yams.gearing.MechanismGearing;
@@ -26,45 +25,8 @@ import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
 import yams.motorcontrollers.local.SparkWrapper;
 
-public class FeederSubsystem extends SubsystemBase {
+public class HotdogSubsystem extends SubsystemBase {
 
-  private SmartMotorControllerConfig smcConfig =
-      new SmartMotorControllerConfig(this)
-          .withControlMode(ControlMode.CLOSED_LOOP)
-          // Feedback Constants (PID Constants)
-          .withClosedLoopController(
-              0, 0, 0, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
-          .withSimClosedLoopController(
-              0, 0, 0, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
-          // Feedforward Constants
-          .withFeedforward(new SimpleMotorFeedforward(0, 0.1, 0))
-          .withSimFeedforward(new SimpleMotorFeedforward(0, 0.1, 0))
-          // Telemetry name and verbosity level
-          .withTelemetry("FeederMotor", TelemetryVerbosity.HIGH)
-          // Gearing from the motor rotor to final shaft.
-          // In this example GearBox.fromReductionStages(3,4) is the same as
-          // GearBox.fromStages("3:1","4:1") which corresponds to the gearbox attached to your
-          // motor.
-          // You could also use .withGearing(12) which does the same thing.
-          .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
-          // Motor properties to prevent over currenting.
-          .withMotorInverted(false)
-          .withIdleMode(MotorMode.COAST)
-          .withStatorCurrentLimit(Amps.of(40));
-
-  private SparkMax feeder = new SparkMax(6, MotorType.kBrushless);
-
-  private SmartMotorController feederSmartMotorController =
-      new SparkWrapper(feeder, DCMotor.getNEO(1), smcConfig);
-
-  private final FlyWheelConfig flywheelConfig =
-      new FlyWheelConfig(feederSmartMotorController)
-          .withDiameter(Inches.of(3.50))
-          .withMass(Pounds.of(0.5))
-          .withUpperSoftLimit(RPM.of(500))
-          .withTelemetry("FeederMech", TelemetryVerbosity.HIGH);
-
-  private FlyWheel Feeder = new FlyWheel(flywheelConfig);
   private SmartMotorControllerConfig HsmcConfig =
       new SmartMotorControllerConfig(this)
           .withControlMode(ControlMode.CLOSED_LOOP)
@@ -89,7 +51,7 @@ public class FeederSubsystem extends SubsystemBase {
           .withIdleMode(MotorMode.COAST)
           .withStatorCurrentLimit(Amps.of(40));
 
-  private SparkMax hotdog = new SparkMax(7, MotorType.kBrushless);
+  private SparkMax hotdog = new SparkMax(8, MotorType.kBrushless);
 
   private SmartMotorController hotdogSmartMotorController =
       new SparkWrapper(hotdog, DCMotor.getNEO(1), HsmcConfig);
@@ -105,34 +67,21 @@ public class FeederSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    Feeder.updateTelemetry();
+
     Hotdog.updateTelemetry();
   }
 
   @Override
   public void simulationPeriodic() {
-    Feeder.simIterate();
-    Hotdog.simIterate();
-  }
 
-  public AngularVelocity feederGetAngularVelocity() {
-    return Feeder.getSpeed();
+    Hotdog.simIterate();
   }
 
   public AngularVelocity hotdogGetAngularVelocity() {
     return Hotdog.getSpeed();
   }
 
-  public Command setFeederAngularVelocity(AngularVelocity angularVelocity) {
-    return Feeder.setSpeed(angularVelocity);
-  }
-
   public Command setHotdogAngularVelocity(AngularVelocity angularVelocity) {
-    return Feeder.setSpeed(angularVelocity);
-  }
-
-  public Command setHotdogFeeder(AngularVelocity angularVelocity) {
-    return Commands.parallel(
-        setFeederAngularVelocity(angularVelocity), setHotdogAngularVelocity(angularVelocity));
+    return Hotdog.setSpeed(angularVelocity);
   }
 }

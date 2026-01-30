d7c1d5c274c3b2fa96d57cc8f2a858164ca618ad
charlie guy <charlie.guy@outlook.com>
2026-01-29 20:58:24 -0500

    Set up the basis for questnav

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -11,8 +11,14 @@ import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import com.pathplanner.lib.path.PathConstraints;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructPublisher;
@@ -25,6 +31,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants;
+import gg.questnav.questnav.PoseFrame;
+import gg.questnav.questnav.QuestNav;
 import java.io.File;
 import java.io.IOException;
 import java.util.Optional;
@@ -33,12 +41,21 @@ import swervelib.SwerveInputStream;
 import swervelib.parser.SwerveParser;
 
 public class SwerveSubsystem extends SubsystemBase {
+  private static final Transform3d ROBOT_TO_QUEST =
+      new Transform3d(/*put in xyz when available */ );
+  private static final Matrix<N3, N1> QUESTNAV_STD_DEVS =
+      VecBuilder.fill(
+          0.02, // Trust down to 2cm in X direction
+          0.02, // Trust down to 2cm in Y direction
+          0.035 // Trust down to 2 degrees rotational
+          );
   public static LinearVelocity MaxDriveSpeed = MetersPerSecond.of(5);
   public static AngularVelocity MaxRotationSpeed = RotationsPerSecond.of(3);
 
   private final SwerveDrive swerveDrive;
   private final StructPublisher<Pose2d> estimatedPosePublisher;
   private final StructPublisher<Pose2d> simulatedPosePublisher;
+  private final QuestNav questNav = new QuestNav();
 
   public SwerveSubsystem() {
 
@@ -63,7 +80,14 @@ public class SwerveSubsystem extends SubsystemBase {
     } catch (IOException ex) {
       throw new RuntimeException(ex);
     }
+    setupQuestNav(new QuestNav());
     setUpAutoPlanner();
+  }
+
+  public void setupQuestNav(QuestNav questNav) {
+
+    Pose3d robotPose3d = new Pose3d(getPose());
+    Pose3d questPose = robotPose3d.transformBy(ROBOT_TO_QUEST);
   }
 
   public void setUpAutoPlanner() {
@@ -146,8 +170,30 @@ public class SwerveSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    swerveDrive.updateOdometry();
-    estimatedPosePublisher.set(swerveDrive.getPose());
+    // Get the latest pose data frames from the Quest
+
+    PoseFrame[] questFrames = questNav.getAllUnreadPoseFrames();
+
+    // Loop over the pose data frames and send them to the pose estimator
+    for (PoseFrame questFrame : questFrames) {
+      // Make sure the Quest was tracking the pose for this frame
+      if (questFrame.isTracking()) {
+        // Get the pose of the Quest
+        Pose3d questPose = questFrame.questPose3d();
+        // Get timestamp for when the data was sent
+        double timestamp = questFrame.dataTimestamp();
+
+        // Transform by the mount pose to get your robot pose
+        Pose3d robotPose = questPose.transformBy(ROBOT_TO_QUEST.inverse());
+
+        // You can put some sort of filtering here if you would like!
+
+        // Add the measurement to our estimator
+        swerveDrive.addVisionMeasurement(robotPose.toPose2d(), timestamp, QUESTNAV_STD_DEVS);
+        swerveDrive.updateOdometry();
+        estimatedPosePublisher.set(swerveDrive.getPose());
+      }
+    }
   }
 
   @Override

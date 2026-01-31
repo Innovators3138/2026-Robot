550a099472fa9b9caae9c967725e1a22abdb0207
charlie guy <charlie.guy@outlook.com>
2026-01-31 10:38:23 -0500

    added questnav offset based on CAD

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -15,6 +15,7 @@ import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.numbers.N1;
@@ -42,7 +43,9 @@ import swervelib.parser.SwerveParser;
 
 public class SwerveSubsystem extends SubsystemBase {
   private static final Transform3d ROBOT_TO_QUEST =
-      new Transform3d(/*put in xyz when available */ );
+      new Transform3d(
+          new edu.wpi.first.math.geometry.Translation3d(0.307, -0.254000, 0.322762),
+          new Rotation3d(0.0, 0.0, 0.0)); // Adjust these values based on your mounting
   private static final Matrix<N3, N1> QUESTNAV_STD_DEVS =
       VecBuilder.fill(
           0.02, // Trust down to 2cm in X direction

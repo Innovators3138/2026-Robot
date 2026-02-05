51c04516b598fdf9ad12523d7954e3ab5990b94f
mrO819 <owen.m.leber@gmail.com>
2026-02-05 18:58:05 -0500

    44 make led turn green when flywheel hits target speed (#45)
    * fix
    
    * added functinality where ti worked yey
    
    * made it actuall7y work
    
    * trying to fix it  finding real speed verses setpoint
    
    * wip
    
    * fixed the command bug
    
    * fix

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -1,25 +1,50 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.RPM;
+
 import com.ctre.phoenix6.controls.SolidColor;
 import com.ctre.phoenix6.hardware.CANdle;
 import com.ctre.phoenix6.signals.RGBWColor;
+import edu.wpi.first.networktables.IntegerPublisher;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDSubsystem extends SubsystemBase {
   private final CANdle candle;
-  private final SolidColor Greencolor = new SolidColor(0, 50);
-  private final RGBWColor Green = new RGBWColor(0, 255, 0, 0);
+  private final SolidColor greenColor = new SolidColor(0, 50);
+  private final SolidColor redColor = new SolidColor(0, 50);
+  private final RGBWColor green = new RGBWColor(0, 255, 0, 0);
+  private final RGBWColor red = new RGBWColor(255, 0, 0, 0);
+  private final ShooterSubsystem shooterSubsystem;
+  private final IntegerPublisher candlePublisher =
+      NetworkTableInstance.getDefault().getIntegerTopic("Subsystems/Ledsubsystem/candle").publish();
 
-  public LEDSubsystem() {
+  public LEDSubsystem(ShooterSubsystem shooterSubsystem) {
+    this.shooterSubsystem = shooterSubsystem;
     this.candle = new CANdle(36);
   }
 
   public Command setToGreen() {
+
     return runOnce(
         () -> {
-          Greencolor.withColor(Green);
-          candle.setControl(Greencolor);
+          var shooterCheck =
+              shooterSubsystem
+                  .getRealAngularVelocity()
+                  .minus(shooterSubsystem.getAngularVelocity().orElse(RPM.of(0)))
+                  .abs(RPM);
+          if (shooterCheck <= 20
+              && shooterSubsystem.getAngularVelocity().orElse(RPM.of(0)).gt(RPM.of(0))) {
+            greenColor.withColor(green);
+            candle.setControl(greenColor);
+            candlePublisher.set(1);
+
+          } else {
+            redColor.withColor(red);
+            candle.setControl(redColor);
+            candlePublisher.set(0);
+          }
         });
   }
 }

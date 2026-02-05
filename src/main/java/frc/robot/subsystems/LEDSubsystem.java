ce7c00b5e5bce830366e36343b6875a281f4d359
mro819 <Owen.m.leber@gmail.com>
2026-02-04 20:33:45 -0500

    fix

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -5,14 +5,20 @@ import static edu.wpi.first.units.Units.RPM;
 import com.ctre.phoenix6.controls.SolidColor;
 import com.ctre.phoenix6.hardware.CANdle;
 import com.ctre.phoenix6.signals.RGBWColor;
+import edu.wpi.first.networktables.IntegerPublisher;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDSubsystem extends SubsystemBase {
   private final CANdle candle;
   private final SolidColor greenColor = new SolidColor(0, 50);
+  private final SolidColor redColor = new SolidColor(0, 50);
   private final RGBWColor green = new RGBWColor(0, 255, 0, 0);
+  private final RGBWColor red = new RGBWColor(255, 0, 0, 0);
   private final ShooterSubsystem shooterSubsystem;
+  private final IntegerPublisher candlePublisher =
+      NetworkTableInstance.getDefault().getIntegerTopic("Subsystems/Ledsubsystem/candle").publish();
 
   public LEDSubsystem(ShooterSubsystem shooterSubsystem) {
     this.shooterSubsystem = shooterSubsystem;
@@ -26,10 +32,18 @@ public class LEDSubsystem extends SubsystemBase {
           var shooterCheck =
               shooterSubsystem
                   .getRealAngularVelocity()
-                  .minus(shooterSubsystem.getAngularVelocity().orElse(RPM.of(0)));
-          if (shooterCheck.lte(RPM.of(20))) {
+                  .minus(shooterSubsystem.getAngularVelocity().orElse(RPM.of(0)))
+                  .abs(RPM);
+          if (shooterCheck <= 20
+              && shooterSubsystem.getAngularVelocity().orElse(RPM.of(0)).gt(RPM.of(0))) {
             greenColor.withColor(green);
             candle.setControl(greenColor);
+            candlePublisher.set(1);
+
+          } else {
+            redColor.withColor(red);
+            candle.setControl(redColor);
+            candlePublisher.set(0);
           }
         });
   }

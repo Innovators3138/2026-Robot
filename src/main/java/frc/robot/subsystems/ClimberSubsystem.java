b7c438ccb3d2a79fe5a42822947437cc459acd58
chargames2011 <91035998+chargames2011@users.noreply.github.com>
2026-02-07 14:55:20 -0500

    added a way to move the climber up and down with the ratchet (#52)

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -7,6 +7,8 @@ import static edu.wpi.first.units.Units.PoundsForce;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.Force;
 import edu.wpi.first.units.measure.Mass;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /* Instantiates the subsystem */
@@ -15,4 +17,21 @@ public class ClimberSubsystem extends SubsystemBase {
   public static final Distance MAX_EXTENSION = Inches.of(9);
   public static final Force SPRING_FORCE = PoundsForce.of(7);
   public static final Mass CLIMBER_MASS = Pounds.of(0.5);
+  private Servo ratchetServo = new Servo(19);
+
+  public Command toggleRatchet() {
+    return runOnce(
+        () -> {
+          if (ratchetServo.get() < 0.5) {
+            ratchetServo.set(1.0);
+
+          } else {
+            ratchetServo.set(0.0);
+          }
+        });
+  }
+
+  public double getRatchetPosition() {
+    return ratchetServo.get();
+  }
 }

28634ddd0a40345032a05a0b685b560b2d71daed
mrO819 <owen.m.leber@gmail.com>
2026-02-07 15:40:28 -0500

    48 3d model moving for elevator (#53)
    * fixed config json
    
    * i have problems
    
    * ok
    
    * it work loq
    
    * eleavtor is on 3d model, cannot move prob because the 2d mechanism makes the ligament longer, is not posible with real things

--- src/main/java/frc/robot/simulation/ClimberSimulator.java
+++ src/main/java/frc/robot/simulation/ClimberSimulator.java
@@ -4,26 +4,42 @@ import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
 
+import java.lang.reflect.Array;
+
+import org.littletonrobotics.junction.mechanism.LoggedMechanism2d;
+import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d;
+
 import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 import frc.robot.subsystems.ClimberSubsystem;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructArrayPublisher;
+import edu.wpi.first.math.geometry.Pose3d;
+import org.littletonrobotics.junction.Logger;
+import java.util.ArrayList;
+
+
 
 public class ClimberSimulator {
   private final ClimberSubsystem climberSubsystem;
-  private final Mechanism2d mech2d;
-  private final MechanismLigament2d climberExtender;
+  private final LoggedMechanism2d mech2d;
+  private final LoggedMechanismLigament2d climberExtender;
+  private final StructArrayPublisher climberPosePublisher;
+  private final ArrayList<Pose3d> climberArrayPose;
 
   public ClimberSimulator(ClimberSubsystem climberSubsystem) {
     this.climberSubsystem = climberSubsystem;
-    this.mech2d = new Mechanism2d(40, 40);
+    this.mech2d = new LoggedMechanism2d(40, 40);
+    climberPosePublisher = NetworkTableInstance.getDefault().getStructArrayTopic("Simulation/ClimberSimulator/ClimberPose", Pose3d.struct).publish();
     var climberRoot = mech2d.getRoot("Climber", 20, 0);
     var climberLigament =
-        new MechanismLigament2d(
+        new LoggedMechanismLigament2d(
             "Climber Ligament",
             ClimberSubsystem.BASE_LENGTH.in(Inches),
             90,
@@ -31,9 +47,13 @@ public class ClimberSimulator {
             new Color8Bit(Color.kAquamarine));
     climberRoot.append(climberLigament);
     this.climberExtender =
-        new MechanismLigament2d("Climber Extender", 0, 0, 4, new Color8Bit(Color.kMaroon));
+        new LoggedMechanismLigament2d("Climber Extender", 0, 0, 4, new Color8Bit(Color.kMaroon));
     climberLigament.append(climberExtender);
+    climberArrayPose = mech2d.generate3dMechanism();
+    Pose3d[] poseArray = climberArrayPose.toArray(new Pose3d[0]);
+    climberPosePublisher.set(poseArray);
     SmartDashboard.putData("ClimberMechanism", mech2d);
+
   }
 
   public void update(Time dt) {

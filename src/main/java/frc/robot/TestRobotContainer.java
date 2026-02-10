61c28766516149b4e6681c5aa87a37b14135fc22
charlie guy <charlie.guy@outlook.com>
2026-02-09 19:24:58 -0500

    Added functionality for testing without removing anything

--- src/main/java/frc/robot/TestRobotContainer.java
+++
@@ -1,7 +0,0 @@
-package frc.robot;
-
-import frc.robot.subsystems.ShooterSubsystem;
-
-public class TestRobotContainer {
-   public final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
-}
--- src/main/java/frc/robot/harnesses/ShooterTestContainer.java
+++ src/main/java/frc/robot/harnesses/ShooterTestContainer.java
@@ -1,4 +1,4 @@
-package frc.robot;
+package frc.robot.harnesses;
 
 import static edu.wpi.first.units.Units.RPM;
 

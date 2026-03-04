aef5eba3bde099b0277945a9d50fd02b228ee340
mro819 <Owen.m.leber@gmail.com>
2026-03-04 18:53:35 -0500

    Refactor LEDSubsystem code for improved readability and formatting and publishing

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -7,54 +7,50 @@ import com.ctre.phoenix6.hardware.CANdle;
 import com.ctre.phoenix6.signals.RGBWColor;
 import edu.wpi.first.networktables.IntegerPublisher;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.LEDConstants;
 
 public class LEDSubsystem extends SubsystemBase {
   private final CANdle candle;
- // private final SolidColor greenColor = new SolidColor(0, 50);
- // private final SolidColor redColor = new SolidColor(0, 50);
+  // private final SolidColor greenColor = new SolidColor(0, 50);
+  // private final SolidColor redColor = new SolidColor(0, 50);
   private final RGBWColor green = new RGBWColor(0, 255, 0, 0);
   private final RGBWColor red = new RGBWColor(255, 0, 0, 0);
   private final ShooterSubsystem shooterSubsystem;
-  private final IntegerPublisher candlePublisher =
-      NetworkTableInstance.getDefault().getIntegerTopic("Subsystems/Ledsubsystem/candle").publish();
+  private final IntegerPublisher flywheelLEDPublisher =
+      NetworkTableInstance.getDefault()
+          .getIntegerTopic("Subsystems/Ledsubsystem/FlywheelLEDs")
+          .publish();
 
   public LEDSubsystem(ShooterSubsystem shooterSubsystem) {
     this.shooterSubsystem = shooterSubsystem;
     this.candle = new CANdle(21);
   }
+
   private double getGetSetpointAccuracyPercentage() {
     var realSpeed = shooterSubsystem.getRealAngularVelocity().in(RPM);
     var setpoint = shooterSubsystem.getAngularVelocitySetpoint().map(s -> s.in(RPM)).orElse(0.0);
-    if (setpoint == 0){
+    if (setpoint == 0) {
       return 0;
     }
-    var percentage = realSpeed/setpoint;
+    var percentage = realSpeed / setpoint;
 
     return percentage;
+  }
 
-    }
-@Override
+  @Override
   public void periodic() {
     var percentage = getGetSetpointAccuracyPercentage();
-    var Led = LEDConstants.LED_NUMBER;
-    var level = (int)(percentage * Led);
-     RGBWColor color;
+    var led = LEDConstants.LED_NUMBER;
+    var level = (int) (percentage * led);
+    RGBWColor color;
     if (percentage > 1) {
-       color = red;
-    }else{
-       color = green;
+      color = red;
+      level = 20;
+    } else {
+      color = green;
     }
     candle.setControl(new SolidColor(8, level + 8).withColor(color));
-
-
+    flywheelLEDPublisher.set(level);
   }
 }
-
-
-
-
-

66b9ceee363cfa6516b995bfebb095dd36fca1a2
Jon Guy <jon.guy@live.com>
2026-03-19 09:34:27 -0400

    Added command to open the intake hopper

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,7 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.AutoCommands;
 import frc.robot.commands.FireCommand;
@@ -21,53 +22,55 @@ public class RobotContainer {
 
   public final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
   public final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
-  public final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
-  public final FeederSubsystem feederSubsystem = new FeederSubsystem();
-  public final HotdogSubsystem hotdogSubsystem = new HotdogSubsystem();
-  public final LEDSubsystem ledSubsystem = new LEDSubsystem(shooterSubsystem, swerveSubsystem);
-  public final CommandXboxController driverXbox = new CommandXboxController(0);
-  public final CommandXboxController operatorXbox = new CommandXboxController(1);
+  public final static IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+    public final FeederSubsystem feederSubsystem = new FeederSubsystem();
+    public final HotdogSubsystem hotdogSubsystem = new HotdogSubsystem();
+    public final LEDSubsystem ledSubsystem = new LEDSubsystem(shooterSubsystem, swerveSubsystem);
+    public final CommandXboxController driverXbox = new CommandXboxController(0);
+    public final CommandXboxController operatorXbox = new CommandXboxController(1);
 
-  public final VisionSubsystem visionSubsystem = new VisionSubsystem(swerveSubsystem);
-  public final SendableChooser<Command> autoChooser;
+    public final VisionSubsystem visionSubsystem = new VisionSubsystem(swerveSubsystem);
+    public final SendableChooser<Command> autoChooser;
 
-  public RobotContainer() {
+    public RobotContainer() {
 
-    // Another option that allows you to specify the default auto by its name
-    autoChooser = AutoBuilder.buildAutoChooser("My Default Auto");
-    configureBindings();
-    AutoCommands.climbChooser.setDefaultOption("Left Climb", "Left Climb");
-    AutoCommands.climbChooser.addOption("Middle Climb", "Middle Climb");
-    AutoCommands.climbChooser.addOption("Right Climb", "Right Climb");
-    AutoCommands.intakeChooser.setDefaultOption("Left Intake", "Left Intake");
-    AutoCommands.intakeChooser.addOption("Depot Intake", "Depot Intake");
-    AutoCommands.intakeChooser.addOption("Right Intake", "Right Intake");
-    SmartDashboard.putData(AutoCommands.climbChooser);
-    SmartDashboard.putData(AutoCommands.intakeChooser);
-  }
+      // Another option that allows you to specify the default auto by its name
+      autoChooser = AutoBuilder.buildAutoChooser("My Default Auto");
+      configureBindings();
+      AutoCommands.climbChooser.setDefaultOption("Left Climb", "Left Climb");
+      AutoCommands.climbChooser.addOption("Middle Climb", "Middle Climb");
+      AutoCommands.climbChooser.addOption("Right Climb", "Right Climb");
+      AutoCommands.intakeChooser.setDefaultOption("Left Intake", "Left Intake");
+      AutoCommands.intakeChooser.addOption("Depot Intake", "Depot Intake");
+      AutoCommands.intakeChooser.addOption("Right Intake", "Right Intake");
+      SmartDashboard.putData(AutoCommands.climbChooser);
+      SmartDashboard.putData(AutoCommands.intakeChooser);
+    }
 
-  private void configureBindings() {
+    private void configureBindings() {
 
-    swerveSubsystem.setDefaultCommand(swerveSubsystem.driveFieldOriented(driverXbox, operatorXbox));
-    hotdogSubsystem.setDefaultCommand(hotdogSubsystem.setHotdogAngularVelocity(RPM.of(0)));
-    shooterSubsystem.setDefaultCommand(shooterSubsystem.setAngularVelocity(RPM.of(0)));
-    intakeSubsystem.setDefaultCommand(intakeSubsystem.setAngularVelocity(RPM.of(0)));
-    feederSubsystem.setDefaultCommand(feederSubsystem.setFeederAngularVelocity(RPM.of(0)));
-    hotdogSubsystem.setDefaultCommand(hotdogSubsystem.setHotdogAngularVelocity(RPM.of(0)));
+      swerveSubsystem.setDefaultCommand(swerveSubsystem.driveFieldOriented(driverXbox, operatorXbox));
+      hotdogSubsystem.setDefaultCommand(hotdogSubsystem.setHotdogAngularVelocity(RPM.of(0)));
+      shooterSubsystem.setDefaultCommand(shooterSubsystem.setAngularVelocity(RPM.of(0)));
+      intakeSubsystem.setDefaultCommand(intakeSubsystem.setAngularVelocity(RPM.of(0)));
+      feederSubsystem.setDefaultCommand(feederSubsystem.setFeederAngularVelocity(RPM.of(0)));
+      hotdogSubsystem.setDefaultCommand(hotdogSubsystem.setHotdogAngularVelocity(RPM.of(0)));
 
-    operatorXbox
-        .rightTrigger()
-        .whileTrue(FireCommand.fire(feederSubsystem, hotdogSubsystem, shooterSubsystem));
-    operatorXbox.a().toggleOnTrue(intakeSubsystem.setAngularVelocity(RPM.of(900)));
+      operatorXbox
+          .rightTrigger()
+          .whileTrue(FireCommand.fire(feederSubsystem, hotdogSubsystem, shooterSubsystem));
+      operatorXbox.a().toggleOnTrue(intakeSubsystem.setAngularVelocity(RPM.of(900)));
 
-    operatorXbox
-        .rightTrigger()
-        .and(operatorXbox.y())
-        .whileTrue(FireCommand.unjam(feederSubsystem, hotdogSubsystem, intakeSubsystem));
+      operatorXbox
+          .rightTrigger()
+          .and(operatorXbox.y())
+          .whileTrue(FireCommand.unjam(feederSubsystem, hotdogSubsystem, intakeSubsystem));
 
-    operatorXbox
-        .leftTrigger(0.5)
-        .whileTrue(FireCommand.targetLock(shooterSubsystem, swerveSubsystem));
+      operatorXbox
+          .leftTrigger(0.5)
+          .whileTrue(FireCommand.targetLock(shooterSubsystem, swerveSubsystem));
+
+      operatorXbox.povDown().whileTrue(Commands.runOnce(() -> RobotContainer.intakeSubsystem.openHopper()));
 
     if (Robot.isSimulation()) {
       driverXbox.start().onTrue(swerveSubsystem.resetSimOdometry());
